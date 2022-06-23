b8d0e2394eb679d9ffa7c5ec65315f39d797af38
Jonathan-Moreno <jonathanpmoreno2015@gmail.com>
2022-06-23 02:18:04 -0700

    completed deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,12 @@ View(MechaCarMPG) #View dataframe
 lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data = MechaCarMPG) #Perform linear regression 
 
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data = MechaCarMPG)) #Use summary function to determine p-value and r-squared
+
+# Suspension coil summary
+
+SuspensionCoilData <- read.csv('Suspension_Coil.csv', check.names = F,stringsAsFactors = F) #Import data
+
+total_summary <- SuspensionCoilData %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) # Use summarize function to get mean, median, variance and std dev
+
+lot_summary <- SuspensionCoilData %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) # Use group_by function and summarize function to create lot summary 
+
