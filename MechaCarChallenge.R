9ece093f3a32d257dddc9f7e569b8e141e5c71a2
Jonathan-Moreno <jonathanpmoreno2015@gmail.com>
2022-06-23 03:03:51 -0700

    completed deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,3 +19,18 @@ total_summary <- SuspensionCoilData %>% summarize(Mean=mean(PSI),Median=median(P
 
 lot_summary <- SuspensionCoilData %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) # Use group_by function and summarize function to create lot summary 
 
+# T-Tests on Suspension Coils
+
+t.test(SuspensionCoilData$PSI,mu=1500) # Perform T-test on all lots
+
+lot_1 <- subset(SuspensionCoilData,Manufacturing_Lot=="Lot1") # Create lot 1 subset
+
+t.test(lot_1$PSI,mu=1500) # Perform T-test for lot 1
+
+lot_2 <- subset(SuspensionCoilData,Manufacturing_Lot=="Lot2") # Create lot 2 subset
+
+t.test(lot_2$PSI,mu=1500) # Perform T-test for lot 2
+
+lot_3 <- subset(SuspensionCoilData,Manufacturing_Lot=="Lot3") # Create lot 3 subset
+
+t.test(lot_3$PSI,mu=1500) # Perform T-test for lot 3
